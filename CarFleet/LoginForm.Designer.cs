7ccb25b5ba9532c70fc64efa0ea0c6f2ecf7ade8
Mateusz Jaruga <UL0253575@edu.uni.lodz.pl>
2023-03-04 16:18:05 +0100

    Added database structure to readme

---
+++ CarFleet/LoginForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace CarFleet
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tBLogin = new System.Windows.Forms.TextBox();
+            this.tBPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tBLogin
+            // 
+            this.tBLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.tBLogin.Location = new System.Drawing.Point(268, 40);
+            this.tBLogin.Name = "tBLogin";
+            this.tBLogin.Size = new System.Drawing.Size(100, 26);
+            this.tBLogin.TabIndex = 0;
+            // 
+            // tBPassword
+            // 
+            this.tBPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.tBPassword.Location = new System.Drawing.Point(268, 110);
+            this.tBPassword.Name = "tBPassword";
+            this.tBPassword.PasswordChar = '*';
+            this.tBPassword.Size = new System.Drawing.Size(100, 26);
+            this.tBPassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(153, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(153, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnLogin.Location = new System.Drawing.Point(207, 174);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(90, 28);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 271);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tBPassword);
+            this.Controls.Add(this.tBLogin);
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tBLogin;
+        private System.Windows.Forms.TextBox tBPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # CarFleet
+
+# Database
+Person:
+•	ID
+•   FirstName
+•	LastName
+•	PhoneNumber
+Users:
+•	ID
+•	UserName
+•	PasswordHash
+•	PersonID
+•	RoleID
+Vehicle:
+•	ID
+•	Manufacturer
+•	Model
+•	ProductionYear
+•	LicensePlate
+•	VinNumber
+•	CreatedOn
+•	CreatedByID
+•	UpdatedOn
+•	UpdatedByID
+VehiclePersonHistory:
+•	ID
+•	VehicleID
+•	PersonID
+•	CreatedOn
+•	CreatedByID
+•	UpdatedOn
+•	UpdatedByID
+VehicleInspection:
+•	ID
+•	VehicleID
+•	DateOfInspection
+•	DateOfNextInspection
+•	CreatedOn
+•	CreatedByID
+•	UpdatedOn
+•	UpdatedByID
+VehicleDescription
+•	ID
+•	VehicleID
+•	Description
+•	CreatedOn
+•	CreatedByID
+•	UpdatedOn
+•	UpdatedByID
+VehcileStatus:
+•	ID
+•	VehicleID
+•	Status
+•	CreatedOn
+•	CreatedByID
