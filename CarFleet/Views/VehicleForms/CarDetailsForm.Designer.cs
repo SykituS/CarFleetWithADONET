f4c92cf98f229fdefebe2f4aca66c64b1d1d5f8a
Mateusz Jaruga <UL0253575@edu.uni.lodz.pl>
2023-03-24 20:39:29 +0100

    Bug Fixing

--- CarFleet/Views/VehicleForms/CarDetailsForm.Designer.cs
+++ CarFleet/Views/VehicleForms/CarDetailsForm.Designer.cs
@@ -49,6 +49,12 @@
             this.LaberWarning = new System.Windows.Forms.Label();
             this.panel7 = new System.Windows.Forms.Panel();
             this.label7 = new System.Windows.Forms.Label();
+            this.BtnOpenStatusForm = new System.Windows.Forms.Button();
+            this.BtnOpenMileageCheckForm = new System.Windows.Forms.Button();
+            this.BtnOpenInspectionForm = new System.Windows.Forms.Button();
+            this.BtnOpenInsurenceForm = new System.Windows.Forms.Button();
+            this.BtnOpenDescriptionForm = new System.Windows.Forms.Button();
+            this.BtnOpenEmployeeToVehicleForm = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.DataGridViewStatusHistory)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.DataGridViewInspectionHistory)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.DataGridViewPersonHistory)).BeginInit();
@@ -68,9 +74,9 @@
             // 
             this.DataGridViewStatusHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.DataGridViewStatusHistory.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.DataGridViewStatusHistory.Location = new System.Drawing.Point(0, 19);
+            this.DataGridViewStatusHistory.Location = new System.Drawing.Point(0, 18);
             this.DataGridViewStatusHistory.Name = "DataGridViewStatusHistory";
-            this.DataGridViewStatusHistory.Size = new System.Drawing.Size(803, 150);
+            this.DataGridViewStatusHistory.Size = new System.Drawing.Size(243, 238);
             this.DataGridViewStatusHistory.TabIndex = 2;
             this.DataGridViewStatusHistory.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.DataGridViewStatusHistory_CellFormatting);
             // 
@@ -80,7 +86,7 @@
             this.DataGridViewInspectionHistory.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.DataGridViewInspectionHistory.Location = new System.Drawing.Point(0, 19);
             this.DataGridViewInspectionHistory.Name = "DataGridViewInspectionHistory";
-            this.DataGridViewInspectionHistory.Size = new System.Drawing.Size(814, 150);
+            this.DataGridViewInspectionHistory.Size = new System.Drawing.Size(821, 150);
             this.DataGridViewInspectionHistory.TabIndex = 3;
             // 
             // DataGridViewPersonHistory
@@ -89,7 +95,7 @@
             this.DataGridViewPersonHistory.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.DataGridViewPersonHistory.Location = new System.Drawing.Point(0, 19);
             this.DataGridViewPersonHistory.Name = "DataGridViewPersonHistory";
-            this.DataGridViewPersonHistory.Size = new System.Drawing.Size(803, 150);
+            this.DataGridViewPersonHistory.Size = new System.Drawing.Size(821, 150);
             this.DataGridViewPersonHistory.TabIndex = 4;
             // 
             // DataGridViewDescriptionHistory
@@ -98,7 +104,7 @@
             this.DataGridViewDescriptionHistory.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.DataGridViewDescriptionHistory.Location = new System.Drawing.Point(0, 19);
             this.DataGridViewDescriptionHistory.Name = "DataGridViewDescriptionHistory";
-            this.DataGridViewDescriptionHistory.Size = new System.Drawing.Size(803, 150);
+            this.DataGridViewDescriptionHistory.Size = new System.Drawing.Size(1657, 150);
             this.DataGridViewDescriptionHistory.TabIndex = 5;
             // 
             // DataGridViewMileageHistory
@@ -124,9 +130,9 @@
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.DataGridViewStatusHistory);
-            this.panel1.Location = new System.Drawing.Point(11, 705);
+            this.panel1.Location = new System.Drawing.Point(1364, 29);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(803, 169);
+            this.panel1.Size = new System.Drawing.Size(243, 256);
             this.panel1.TabIndex = 8;
             // 
             // label1
@@ -199,9 +205,9 @@
             this.panel3.AutoSize = true;
             this.panel3.Controls.Add(this.label2);
             this.panel3.Controls.Add(this.DataGridViewInspectionHistory);
-            this.panel3.Location = new System.Drawing.Point(854, 705);
+            this.panel3.Location = new System.Drawing.Point(11, 518);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(814, 169);
+            this.panel3.Size = new System.Drawing.Size(821, 169);
             this.panel3.TabIndex = 15;
             // 
             // panel4
@@ -209,9 +215,9 @@
             this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.panel4.Controls.Add(this.label4);
             this.panel4.Controls.Add(this.DataGridViewDescriptionHistory);
-            this.panel4.Location = new System.Drawing.Point(11, 518);
+            this.panel4.Location = new System.Drawing.Point(11, 705);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(803, 169);
+            this.panel4.Size = new System.Drawing.Size(1657, 169);
             this.panel4.TabIndex = 16;
             // 
             // panel5
@@ -221,7 +227,7 @@
             this.panel5.Controls.Add(this.label3);
             this.panel5.Location = new System.Drawing.Point(11, 328);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(803, 169);
+            this.panel5.Size = new System.Drawing.Size(821, 169);
             this.panel5.TabIndex = 17;
             // 
             // panel6
@@ -257,15 +263,81 @@
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(13, 10);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.Size = new System.Drawing.Size(82, 13);
             this.label7.TabIndex = 0;
-            this.label7.Text = "label7";
+            this.label7.Text = "labelVehicleInfo";
+            // 
+            // BtnOpenStatusForm
+            // 
+            this.BtnOpenStatusForm.Location = new System.Drawing.Point(414, 123);
+            this.BtnOpenStatusForm.Name = "BtnOpenStatusForm";
+            this.BtnOpenStatusForm.Size = new System.Drawing.Size(206, 23);
+            this.BtnOpenStatusForm.TabIndex = 21;
+            this.BtnOpenStatusForm.Text = "Change Status";
+            this.BtnOpenStatusForm.UseVisualStyleBackColor = true;
+            this.BtnOpenStatusForm.Click += new System.EventHandler(this.BtnOpenStatusForm_Click);
+            // 
+            // BtnOpenMileageCheckForm
+            // 
+            this.BtnOpenMileageCheckForm.Location = new System.Drawing.Point(414, 155);
+            this.BtnOpenMileageCheckForm.Name = "BtnOpenMileageCheckForm";
+            this.BtnOpenMileageCheckForm.Size = new System.Drawing.Size(206, 23);
+            this.BtnOpenMileageCheckForm.TabIndex = 22;
+            this.BtnOpenMileageCheckForm.Text = "New mileage check";
+            this.BtnOpenMileageCheckForm.UseVisualStyleBackColor = true;
+            this.BtnOpenMileageCheckForm.Click += new System.EventHandler(this.BtnOpenMileageCheckForm_Click);
+            // 
+            // BtnOpenInspectionForm
+            // 
+            this.BtnOpenInspectionForm.Location = new System.Drawing.Point(414, 187);
+            this.BtnOpenInspectionForm.Name = "BtnOpenInspectionForm";
+            this.BtnOpenInspectionForm.Size = new System.Drawing.Size(206, 23);
+            this.BtnOpenInspectionForm.TabIndex = 23;
+            this.BtnOpenInspectionForm.Text = "New inspection";
+            this.BtnOpenInspectionForm.UseVisualStyleBackColor = true;
+            this.BtnOpenInspectionForm.Click += new System.EventHandler(this.BtnOpenInspectionForm_Click);
+            // 
+            // BtnOpenInsurenceForm
+            // 
+            this.BtnOpenInsurenceForm.Location = new System.Drawing.Point(414, 221);
+            this.BtnOpenInsurenceForm.Name = "BtnOpenInsurenceForm";
+            this.BtnOpenInsurenceForm.Size = new System.Drawing.Size(206, 23);
+            this.BtnOpenInsurenceForm.TabIndex = 24;
+            this.BtnOpenInsurenceForm.Text = "New Insurence";
+            this.BtnOpenInsurenceForm.UseVisualStyleBackColor = true;
+            this.BtnOpenInsurenceForm.Click += new System.EventHandler(this.BtnOpenInsurenceForm_Click);
+            // 
+            // BtnOpenDescriptionForm
+            // 
+            this.BtnOpenDescriptionForm.Location = new System.Drawing.Point(414, 256);
+            this.BtnOpenDescriptionForm.Name = "BtnOpenDescriptionForm";
+            this.BtnOpenDescriptionForm.Size = new System.Drawing.Size(206, 23);
+            this.BtnOpenDescriptionForm.TabIndex = 25;
+            this.BtnOpenDescriptionForm.Text = "New description";
+            this.BtnOpenDescriptionForm.UseVisualStyleBackColor = true;
+            this.BtnOpenDescriptionForm.Click += new System.EventHandler(this.BtnOpenDescriptionForm_Click);
+            // 
+            // BtnOpenEmployeeToVehicleForm
+            // 
+            this.BtnOpenEmployeeToVehicleForm.Location = new System.Drawing.Point(414, 97);
+            this.BtnOpenEmployeeToVehicleForm.Name = "BtnOpenEmployeeToVehicleForm";
+            this.BtnOpenEmployeeToVehicleForm.Size = new System.Drawing.Size(206, 23);
+            this.BtnOpenEmployeeToVehicleForm.TabIndex = 26;
+            this.BtnOpenEmployeeToVehicleForm.Text = "Add employee to vehicle";
+            this.BtnOpenEmployeeToVehicleForm.UseVisualStyleBackColor = true;
+            this.BtnOpenEmployeeToVehicleForm.Click += new System.EventHandler(this.BtnOpenEmployeeToVehicleForm_Click);
             // 
             // CarDetailsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1680, 880);
+            this.Controls.Add(this.BtnOpenEmployeeToVehicleForm);
+            this.Controls.Add(this.BtnOpenDescriptionForm);
+            this.Controls.Add(this.BtnOpenInsurenceForm);
+            this.Controls.Add(this.BtnOpenInspectionForm);
+            this.Controls.Add(this.BtnOpenMileageCheckForm);
+            this.Controls.Add(this.BtnOpenStatusForm);
             this.Controls.Add(this.panel7);
             this.Controls.Add(this.LaberWarning);
             this.Controls.Add(this.panel5);
@@ -324,5 +396,11 @@
         private System.Windows.Forms.Label LaberWarning;
         private System.Windows.Forms.Panel panel7;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button BtnOpenStatusForm;
+        private System.Windows.Forms.Button BtnOpenMileageCheckForm;
+        private System.Windows.Forms.Button BtnOpenInspectionForm;
+        private System.Windows.Forms.Button BtnOpenInsurenceForm;
+        private System.Windows.Forms.Button BtnOpenDescriptionForm;
+        private System.Windows.Forms.Button BtnOpenEmployeeToVehicleForm;
     }
 }
