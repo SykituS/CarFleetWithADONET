a80da05324502870fc305f3da8b2d69eb4702939
Mateusz Jaruga <UL0253575@edu.uni.lodz.pl>
2023-03-30 22:28:55 +0200

    Bug Fixing

--- CarFleet/Views/VehicleForms/AddNewPersonToVehicleForm.Designer.cs
+++ CarFleet/Views/VehicleForms/AddNewPersonToVehicleForm.Designer.cs
@@ -31,16 +31,17 @@
             this.LabelWarning = new System.Windows.Forms.Label();
             this.BtnAddInspection = new FontAwesome.Sharp.IconButton();
             this.BtnBack = new FontAwesome.Sharp.IconButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // LabelWarning
             // 
             this.LabelWarning.AutoSize = true;
             this.LabelWarning.ForeColor = System.Drawing.Color.Red;
-            this.LabelWarning.Location = new System.Drawing.Point(485, 270);
-            this.LabelWarning.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LabelWarning.Location = new System.Drawing.Point(558, 330);
             this.LabelWarning.Name = "LabelWarning";
-            this.LabelWarning.Size = new System.Drawing.Size(91, 16);
+            this.LabelWarning.Size = new System.Drawing.Size(73, 13);
             this.LabelWarning.TabIndex = 24;
             this.LabelWarning.Text = "LabelWarning";
             // 
@@ -55,9 +56,10 @@
             this.BtnAddInspection.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.BtnAddInspection.IconSize = 32;
             this.BtnAddInspection.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.BtnAddInspection.Location = new System.Drawing.Point(456, 377);
+            this.BtnAddInspection.Location = new System.Drawing.Point(342, 306);
+            this.BtnAddInspection.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BtnAddInspection.Name = "BtnAddInspection";
-            this.BtnAddInspection.Size = new System.Drawing.Size(163, 45);
+            this.BtnAddInspection.Size = new System.Drawing.Size(122, 37);
             this.BtnAddInspection.TabIndex = 28;
             this.BtnAddInspection.Text = "Create";
             this.BtnAddInspection.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -74,27 +76,37 @@
             this.BtnBack.IconColor = System.Drawing.Color.White;
             this.BtnBack.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.BtnBack.IconSize = 32;
-            this.BtnBack.Location = new System.Drawing.Point(291, 377);
+            this.BtnBack.Location = new System.Drawing.Point(218, 306);
+            this.BtnBack.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BtnBack.Name = "BtnBack";
-            this.BtnBack.Size = new System.Drawing.Size(145, 45);
+            this.BtnBack.Size = new System.Drawing.Size(109, 37);
             this.BtnBack.TabIndex = 29;
             this.BtnBack.Text = "Back";
             this.BtnBack.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.BtnBack.UseVisualStyleBackColor = false;
             this.BtnBack.Click += new System.EventHandler(this.BtnBack_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(108, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(438, 150);
+            this.dataGridView1.TabIndex = 30;
+            // 
             // AddNewPersonToVehicleForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.BtnBack);
             this.Controls.Add(this.BtnAddInspection);
             this.Controls.Add(this.LabelWarning);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "AddNewPersonToVehicleForm";
             this.Text = "AddNewPersonToVehicleForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,5 +116,6 @@
         private System.Windows.Forms.Label LabelWarning;
         private FontAwesome.Sharp.IconButton BtnAddInspection;
         private FontAwesome.Sharp.IconButton BtnBack;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
